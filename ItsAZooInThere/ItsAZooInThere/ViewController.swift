04481578c2da7b253e24571b1be9fbc8ba3e47e4
BF <blake@fleishergroup.com>
2019-04-13 12:15:39 -0500

    need to add sound

--- ItsAZooInThere/ItsAZooInThere/ViewController.swift
+++ ItsAZooInThere/ItsAZooInThere/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
     
@@ -17,46 +17,38 @@ class ViewController: UIViewController {
     var animals : [Animal] = []
     
     
+    @objc func buttonTapped(_ sender:UIButton) {
+        let animal = animals[sender.tag]
+        let alertController = UIAlertController(
+            title: animal.name,
+            message: "Age: \(animal.age), Species: \(animal.species)",
+            preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(
+            title: "Okay",
+            style: .default
+        ))
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+        
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.delegate = self
+        
         scrollView.contentSize = CGSize(width: 1125, height: 500)
-
         
-        var lionSound: AVAudioPlayer?
         let pathLion = Bundle.main.path(forResource: "lionSound.mp3", ofType: nil)!
-        let urlLion = URL(fileURLWithPath: pathLion)
         
-        do {
-            lionSound = try AVAudioPlayer(contentsOf: urlLion)
-            lionSound?.play()
-        } catch {
-            // couldn't load file :(
-        }
-        
-        
-        var tigerSound: AVAudioPlayer?
         let pathTiger = Bundle.main.path(forResource: "tigerSound.mp3", ofType: nil)!
-        let urlTiger = URL(fileURLWithPath: pathTiger)
-        
-        do {
-            tigerSound = try AVAudioPlayer(contentsOf: urlTiger)
-            tigerSound?.play()
-        } catch {
-            // couldn't load file :(
-        }
         
         
-        var bearSound: AVAudioPlayer?
         let pathBear = Bundle.main.path(forResource: "bearSound.mp3", ofType: nil)!
-        let urlBear = URL(fileURLWithPath: pathBear)
-        
-        do {
-            bearSound = try AVAudioPlayer(contentsOf: urlBear)
-            bearSound?.play()
-        } catch {
-            // couldn't load file :(
-        }
         
         
         let lionObj = Animal(name: "Simba", species: "Lion", age: 24, image: UIImage(named: "lionPic")!, soundPath: pathLion)
@@ -71,6 +63,52 @@ class ViewController: UIViewController {
         
         let shuffled = animals.shuffled()
         //https://www.hackingwithswift.com/example-code/arrays/how-to-randomize-the-order-of-an-array-shuffle-and-shuffled
+        
+        
+        var xVal = 375/2
+        var imageX = 0
+        print(animals.count)
+        for (index, animal) in animals.enumerated() {
+            let button = UIButton(type: .system)
+            button.setTitle(animal.name, for: .normal)
+            
+            button.tag = index
+            
+            button.addTarget(self, action: #selector(buttonTapped(_:)), for: UIControl.Event.touchUpInside)
+            
+            button.frame = CGRect(x: xVal, y: 0, width: (375/2 - 140), height: 30)
+            
+            //button.backgroundColor = UIColor.blue
+            scrollView.addSubview(button)
+            
+            let animalImage = UIImageView(frame: CGRect(x: imageX, y: 200, width: 375, height: 250))
+            xVal += 375
+            imageX += 375
+            
+            animalImage.image = animal.image
+            scrollView.addSubview(animalImage)
+            
+            
+            
+        }
+
+        
+        
+        
+//        let button1 = UIButton(type: .system)
+//        button1.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+//        button1.backgroundColor = UIColor.blue
+//        scrollView.addSubview(button1)
+//
+//        let button2 = UIButton(type: .system)
+//        button2.frame = CGRect(x: 80, y: 0, width: 30, height: 30)
+//        button2.backgroundColor = UIColor.blue
+//        scrollView.addSubview(button2)
+//
+        
+        
+        
+       
         
     }
 
